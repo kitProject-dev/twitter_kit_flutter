d474e5c0a4e37bdd303c6ec23d50e0c2173a90de
KoheiYamashita <k-yamashita@kitproject.info>
2020-04-11 23:02:08 +0900

    Fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,24 +6,11 @@ import 'package:twitter_kit/twitter_kit.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Twitter _twitter;
-  Tweet _tweet;
-
-  @override
-  void initState() {
-    super.initState();
-    // TODO Your key.
-    _twitter = Twitter("", "");
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // TODO Your key.
+    final twitter = Twitter("", "", logging: true);
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -31,25 +18,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           child: FutureBuilder(
-            future: _twitter.initialize(),
+            future: twitter.initialize(),
             builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
               if (snapshot.hasData) {
-                if (snapshot.data) {
-                  if (_tweet == null) {
-                    _requestShowTweet();
-                  }
-                  return Text(
-                      _tweet != null ? jsonEncode(_tweet.toJson()) : "ready");
-                } else {
-                  return RaisedButton(
-                      child: const Text('Sign In With Twitter.'),
-                      onPressed: () async {
-                        final twitterLoginStatus = await _twitter.login();
-                        if (twitterLoginStatus) {
-                          setState(() {});
-                        }
-                      });
-                }
+                return Body(twitter, snapshot.data);
               } else {
                 return Container();
               }
@@ -59,11 +31,59 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
+
+class Body extends StatefulWidget {
+  Body(this._twitter, this.isSessionActive);
+
+  final Twitter _twitter;
+  final bool isSessionActive;
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  bool _isSessionActive;
+  Tweet _tweet;
+
+  @override
+  void initState() {
+    _isSessionActive = widget.isSessionActive;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isSessionActive) {
+      if (_tweet == null) {
+        _requestShowTweet();
+      }
+      return InkWell(
+        onTap: () async {
+          await widget._twitter.logout();
+          setState(() {
+            _isSessionActive = false;
+          });
+        },
+        child: Text(_tweet != null ? jsonEncode(_tweet.toJson()) : "ready"),
+      );
+    } else {
+      return RaisedButton(
+          child: const Text('Sign In With Twitter.'),
+          onPressed: () async {
+            final twitterLoginStatus = await widget._twitter.login();
+            if (twitterLoginStatus) {
+              setState(() {
+                _isSessionActive = true;
+              });
+            }
+          });
+    }
+  }
 
   Future<void> _requestShowTweet() async {
-    _twitter.statusesService
-        .show(1242645624106807297)
-        .then((response) {
+    widget._twitter.statusesService.show(1242645624106807297).then((response) {
       if (response.isSuccessful) {
         setState(() {
           _tweet = response.body;
